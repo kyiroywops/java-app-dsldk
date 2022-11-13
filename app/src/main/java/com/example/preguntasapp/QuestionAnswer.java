c46918fa4f8ba872128a46a0e7e5a9e8fa8c4ea1
Kylian Leroy <aleexisazul@gmail.com>
2022-11-13 19:26:02 -0300

    Main activity

---
+++ app/src/main/java/com/example/preguntasapp/QuestionAnswer.java
@@ -0,0 +1,32 @@
+package com.example.preguntasapp;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+            "Quien escribio la Odisea?",
+            "Cual es la capital de Chile?",
+            "Cual es la moneda oficial en Chile?",
+            "Que cantidad de huesos tiene el cuerpo humano?"
+
+    };
+
+    public static String choices[][] = {
+            {"Homero","Socrates","Aristoteles","Arturo Prat"},
+            {"Buenos Aires","Madrid","Paris","Santiago"},
+            {"Euro","Peso","Dolar","No tiene"},
+            {"121","125","412","206"}
+
+
+
+    };
+
+    public static String correctAnswers [] = {
+            "Homero",
+            "Santiago",
+            "Peso",
+            "206"
+
+
+    };
+
+}
